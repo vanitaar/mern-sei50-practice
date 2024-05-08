4f985c69d65e17c9c91767d2b000c871dc03838f
Vanitaa Rai <vanitrai@gmail.com>
2024-05-08 16:02:51 +0800

    feat vite config

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, "dist", "vite.svg")));
 app.use(express.static(path.join(__dirname, "dist")));
 
+// Put API routes here, before the "catch all" route
+app.get("/api", (req, res) => {
+  res.json({ hello: "world" });
+});
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
