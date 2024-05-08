0be8efa402aa4bb9363080993fdf9dac5ddf5e96
Vanitaa Rai <vanitrai@gmail.com>
2024-05-08 11:32:22 +0800

    feat add express server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use(express.json());
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, "dist", "vite.svg")));
+app.use(express.static(path.join(__dirname, "dist")));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
