501b300802f216a92bc0af2beb0f9855b5494f9d
Vanitaa Rai <vanitrai@gmail.com>
2024-05-09 10:53:16 +0800

    feat change console log to debug in config/database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const debug = require("debug")("mern:config:database");
 
 mongoose.set("debug", true); //for debug
 mongoose.connect(process.env.DATABASE_URL);
@@ -6,5 +7,5 @@ mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 
 db.on("connected", function () {
-  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+  debug(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
