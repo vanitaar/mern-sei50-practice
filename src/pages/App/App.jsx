04052f97bbff4062be5db9c857f60944b64d4a4c
Vanitaa Rai <vanitrai@gmail.com>
2024-05-09 11:04:35 +0800

    feat create draft pages; import pages in app; setState in app; eslint disable comment

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,22 @@
 import debug from "debug";
+import { useState } from "react";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+
 const log = debug("mern:pages:App:App");
 
 function App() {
-  log("hello App"); //to test debug log
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState();
+  log("user %o", user);
+
   return (
     <>
       <main className="App">App</main>
+      <AuthPage />
+      <NewOrderPage />
+      <OrderHistoryPage />
     </>
   );
 }
